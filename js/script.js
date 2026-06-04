b798a8edb089786130d21b3072ecf028bead01a0
ragul <ragulragul84503@gmail.com>
2026-06-04 15:40:04 +0530

    css completed & js update

---
+++ js/script.js
@@ -0,0 +1,88 @@
+// var a = 10; //reinitialize & redeclare
+// let b = 20; //reinitialize
+// const c = 30;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// var num = 10;
+// var str = "ragul";
+// var undefine;
+// var Null = null;
+// var boolean = true;
+
+// console.log( typeof num);
+// console.log( typeof str);
+// console.log( typeof undefine);
+// console.log( typeof Null);
+// console.log( typeof boolean);
+
+// var bigInt = 123n;
+// var symbol = Symbol('li');
+
+// console.log( typeof bigInt);
+// console.log(typeof symbol);
+
+// var arr = [10, 20, 30, 40,]
+// console.log(typeof arr, arr);
+
+// var obj = {
+//     name: "ragul",
+//     dept: ["ct"]
+// };
+// console.log(typeof obj, obj);
+
+//arthimatic operater(+,-,%,*,/)
+
+// var a = 10;
+// var b = 20;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b)
+// console.log(a % b);
+// console.log(a / b);
+
+// //logical operater(&&,||,!)
+
+// var a = true;
+// var b = false;
+
+// console.log(a&&b);
+// console.log(a||b);
+// console.log(!a);
+// console.log("relational operater(<,>,<=,>=,==,!=,===,!==");
+
+// a = 10;
+// b = 20;
+
+// console.log(a<=b);
+// console.log(a>=b);
+// console.log(a==b);
+// console.log(a<b);
+// console.log(a!=b);
+// console.log(b==a);
+// console.log(b>a);
+
+// a = 2;
+
+//  console.log(a**a);
+
+// //assingment operter(=,==,-=,%=,*=,**=);
+//  a = 10;
+//  b = 20;
+
+//  a = a+b;//30
+//  a += b;
+// console.log(a);
+// a -= b;
+// console.log(a);
+
+//unary operation(++,--)
+
+a = 10;
+console.log(a++)
+console.log(a--)
+console.log(--a)
+console.log(++a)
